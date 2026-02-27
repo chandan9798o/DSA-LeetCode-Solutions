fdee67ab7f290dd953e7765099b1d04370b1c768
Chandan Vishwakarma <chandank347711@gmail.com>
2026-02-27 18:57:20 +0530

    Add threeSumClosest method for closest sum calculation
    Implemented threeSumClosest method to find the closest sum to a target using a two-pointer approach.

---
+++ Arrays/3Sum Closet.java
@@ -0,0 +1,37 @@
+//Platform-LeetCode
+//Approach-Standard Two Pointer
+//Time-complexity - o(n^2)
+//Space-Complexity - o(1)
+
+//Code - 
+import java.util.Arrays;
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums); 
+        int closestSum = nums[0] + nums[1] + nums[2]; // Initialize with first three
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int currentSum = nums[i] + nums[left] + nums[right];
+
+                if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
+                    closestSum = currentSum;
+                }
+
+ 
+                if (currentSum < target) {
+                    left++;
+                } else if (currentSum > target) {
+                    right--;
+                } else {
+                    // Exact match
+                    return target;
+                }
+            }
+        }
+        return closestSum;
+    }
+}
