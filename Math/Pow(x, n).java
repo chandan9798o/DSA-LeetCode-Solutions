bd9b3e79d534e56da628d8056692bc14d4486f88
Chandan Vishwakarma <chandank347711@gmail.com>
2026-03-02 20:41:10 +0530

    Added Pow(x, n) solution

---
+++ Math/Pow(x, n).java
@@ -0,0 +1,11 @@
+//platform:LeetCode
+//Approach:Exponentiation by squaring
+//Time Complexity : o(logn)
+//Space Complexity: o(1)
+//Code---
+class Solution {
+    public double myPow(double x, int n) {
+        return (Math.pow(x, n));
+
+    }
+}
