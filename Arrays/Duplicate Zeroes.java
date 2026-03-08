da669fd735f610246e57f6bc696e1fe5c4e73148
Chandan Vishwakarma <chandank347711@gmail.com>
2026-03-08 09:42:19 +0530

    Add method to duplicate zeroes in an array
    Implemented a method to duplicate zeroes in an array by shifting elements.

---
+++ Arrays/Duplicate Zeroes.java
@@ -0,0 +1,20 @@
+//Approach:Simple Shifting
+//Platform:LeetCode
+//Time Complexity:O(n^2)
+//Space Complexity:O(1)
+//Code--
+class solution{
+  public int duplicateZeroes(int[] arr){
+    for(int i=0; i<arr.length; i++){
+      if(arr[i] == 0){
+        for(int j=arr.length-1; j>i; j--){
+          arr[j] = arr[j+1];
+        }
+        if(i+1 < arr.length){
+          arr[i+1] = 0;
+        }
+        i++;
+      }
+    }
+  }
+}
