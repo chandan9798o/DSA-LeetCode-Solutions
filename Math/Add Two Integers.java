e7541fe1b0f6897aadc67239b1d835d7417a75d9
Chandan Vishwakarma <chandank347711@gmail.com>
2026-03-12 16:31:50 +0530

    Add solution for summing two integers
    Implement a solution to add two integers with O(1) time and space complexity.

---
+++ Math/Add Two Integers.java
@@ -0,0 +1,10 @@
+//Platform:LeetCode
+//Approach:Simply adding the two variable 
+//Time complexity:O(1)
+//Space Complexity:)O(1)
+//Code--
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1+num2;
+    }
+}
