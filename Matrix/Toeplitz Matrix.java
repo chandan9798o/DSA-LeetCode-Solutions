8da82f11ee498dcbbca14b78cde8a949b46a39bb
Chandan Vishwakarma <chandank347711@gmail.com>
2026-03-18 19:23:13 +0530

    Create Toeplitz Matrix.java

---
+++ Matrix/Toeplitz Matrix.java
@@ -0,0 +1,19 @@
+//Platoform:LeetCode
+//Approach:Linear search technique used
+//Time Complexity:O(m*n)
+//Space Complexity:O(1)
+//Code--
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for(int i=1; i<m; i++){
+            for(int j = 1; j<n; j++){
+                if(matrix[i][j] != matrix[i-1][j-1]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
