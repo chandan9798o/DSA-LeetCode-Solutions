787f94ffeb8436273024f3a928847fe822079e0e
Chandan Vishwakarma <chandank347711@gmail.com>
2026-06-09 13:54:46 +0530

    Add method to find odd occurring element in array
    Implemented a method to find the element with odd occurrences in an array using a HashMap.

---
+++ Arrays/One odd Occuring.java
@@ -0,0 +1,21 @@
+//Platform:GeeksForGeeks
+//Approach:We use Map function to count the frequency of each element of the given array. And after that we try to know the odd frequency of each 
+//entry value using for each loop of map function. If it contain the element with odd occurence we return it as a output.
+//Time Complexity:O(n)
+//Space Compelxity:O(n)
+class Solution {
+    // Method to find the element with odd occurrence in given array
+    int getOddOccurrence(int[] arr) {
+       Map<Integer, Integer> map = new HashMap<>();
+       int val = 0;
+       for(int n : arr){
+           map.put(n, map.getOrDefault(n, 0)+1);
+       }
+       for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+           if(entry.getValue() % 2 != 0){
+               val = entry.getKey();
+           }
+       }
+       return val;
+    }
+}
