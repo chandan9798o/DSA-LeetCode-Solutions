ab91f7bbeee13b69510023e09c64994c1460d971
Chandan Vishwakarma <chandank347711@gmail.com>
2026-07-21 12:32:39 +0530

    Add maxAreaOfIsland solution using DFS
    Implement maxAreaOfIsland function using DFS to calculate the maximum area of islands in a grid.

---
+++ Matrix/Max Area of Island.java
@@ -0,0 +1,55 @@
+//Platform:LeetCode
+//Approach:Matrix DFS using recursion
+//Time Complexity:O(m*n)
+//Space Complexity:O(m*n)
+
+//Code-
+class Solution {
+    public int maxAreaOfIsland(int[][] grid) {
+
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int maxArea = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+
+                if (grid[i][j] == 1) {
+
+                    int area = dfs(grid, i, j);
+
+                    maxArea = Math.max(maxArea, area);
+                }
+            }
+        }
+
+        return maxArea;
+    }
+
+    private int dfs(int[][] grid, int i, int j) {
+
+        // Base case
+        if (i < 0 ||
+            i >= grid.length ||
+            j < 0 ||
+            j >= grid[0].length ||
+            grid[i][j] == 0) {
+
+            return 0;
+        }
+        grid[i][j] = 0;
+        int area = 1;
+
+        area += dfs(grid, i - 1, j);
+
+        area += dfs(grid, i + 1, j);
+
+        area += dfs(grid, i, j - 1);
+
+        area += dfs(grid, i, j + 1);
+
+        return area;
+    }
+}
