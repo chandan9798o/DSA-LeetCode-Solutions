0d853b6aba54768051e0f4f2f54ed9c79bdadd11
Chandan Vishwakarma <chandank347711@gmail.com>
2026-08-07 12:25:06 +0530

    Add insertAtEnd method to Array End Insert class
    Implement method to insert an element at the end of an ArrayList.

---
+++ Arrays/Array End Insert.java
@@ -0,0 +1,11 @@
+//Platform:GeeksForGeeks
+//Approach: Just write the arr.add(val) which is the given value in question. It will add the element in last position of arrayList.
+//Time Complexity:O(1)
+//Space Complexity:O(1)
+
+//Code--
+class Solution {
+    public void insertAtEnd(ArrayList<Integer> arr, int val) {
+       arr.add(val);
+    }
+}
