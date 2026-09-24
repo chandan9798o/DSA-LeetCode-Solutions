284f33f973f3b8afb71bb19042b6fd6b6ee71757
Chandan Vishwakarma <chandank347711@gmail.com>
2026-09-24 13:21:02 +0530

    Add solution for longest common prefix problem
    Implement brute force approach to find longest common prefix.

---
+++ Strings/Longest Common Substring.java
@@ -0,0 +1,24 @@
+//Platform:GeeksForGeeks
+//Approach:Brute force Attack
+//Time Complexity = O(m * n)
+//Space Complexity = O(1)
+
+//Code 
+class Solution {
+    public String longestCommonPrefix(String arr[]) {
+
+        for (int i = 0; i < arr[0].length(); i++) {
+
+            char ch = arr[0].charAt(i);
+
+            for (int j = 1; j < arr.length; j++) {
+
+                if (i >= arr[j].length() || arr[j].charAt(i) != ch) {
+                    return arr[0].substring(0, i);
+                }
+            }
+        }
+
+        return arr[0];
+    }
+}
